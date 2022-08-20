7edef3225755a7c23606575f7f7841fad3775e36
AntonYakavenka <doomanton@gmail.com>
2022-08-20 19:33:58 +0300

    gh pages add

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import * as serviceWorker from './serviceWorker'
 import App from './app/App'
 import {store} from './app/store'
 import {Provider} from 'react-redux'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </Provider>, document.getElementById('root'))
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <App/>
+      </HashRouter>
+    </Provider>, document.getElementById('root'))
 }
 
 rerenderEntireTree()
@@ -26,7 +26,7 @@ serviceWorker.unregister()
 
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
-    module.hot.accept('./app/App', () => {
-        rerenderEntireTree();
-    })
+  module.hot.accept('./app/App', () => {
+    rerenderEntireTree()
+  })
 }
