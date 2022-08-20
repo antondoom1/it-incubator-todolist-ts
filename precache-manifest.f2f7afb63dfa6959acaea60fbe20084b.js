575af32815c2718a4717e23212f9856f26eb8576
AntonYakavenka <doomanton@gmail.com>
2022-08-20 19:35:24 +0300

    Updates

---
+++ precache-manifest.f2f7afb63dfa6959acaea60fbe20084b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2df7e9f603c72aa155a1b5f5c1d8937",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "650437253450f4c5fc02",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "c54787b64f522eebf76b",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.985f8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650437253450f4c5fc02",
+    "url": "/todolist/static/js/main.f69e47a7.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
